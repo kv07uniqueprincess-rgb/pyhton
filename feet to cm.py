71e438116f5c7953c4c499abd59a4e559b7ad261
kv07uniqueprincess-rgb <kv07uniqueprincess@gamil.com>
2026-02-07 01:53:23 -0800

    runtype

---
+++ feet to cm.py
@@ -0,0 +1,5 @@
+ft=30.48
+cm=float(input("Enter centi feet:"))
+ans=cm*ft
+print("total cm's:",ans)
+      
---
+++ run type.py
@@ -0,0 +1,8 @@
+m1=int(input("Enter mark1:"))
+m2=int(input("Enter mark2:"))
+m3=int(input("Enter mark3:"))
+m4=int(input("Enter mark4:"))
+m5=int(input("Enter mark5:"))
+Total =  m1+m2+m3+m4+m5
+print ("Total mark:",Total)
+
